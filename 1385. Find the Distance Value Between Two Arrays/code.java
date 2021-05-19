1504f65808f89909798869c19d164028275f0ccb
Devansh Koolwal <dkoolwal7@gmail.com>
2021-05-19 07:33:39 +0530

    1385. Find the Distance Value Between Two Arrays

---
+++ 1385. Find the Distance Value Between Two Arrays/code.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        int ans=0;
+        
+        for(int i=0;i<arr1.length;i++){
+            boolean a=true;
+            for(int j=0;j<arr2.length;j++){
+                if(Math.abs(arr1[i]-arr2[j])<=d){
+                    a=false;
+                    break;
+                }
+            }
+            if(a==true){
+                ans++;            
+            }
+        }
+        return ans;
+    }
+}
