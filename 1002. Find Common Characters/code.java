93ee41289dca0d8e5ce8f444942a5c15aff834c6
Devansh Koolwal <dkoolwal7@gmail.com>
2021-06-16 22:50:31 +0530

    1002. Find Common Characters

---
+++ 1002. Find Common Characters/code.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> commonChars(String[] words) {
+        List<String> ans=new ArrayList<String>();
+        for(char a:words[0].toCharArray()){
+            boolean flag=false;
+            for(int i=1;i<words.length;i++){
+                int index=words[i].indexOf(a);
+                if(index>=0){
+                    char ch[]=words[i].toCharArray();
+                    ch[index]=0;
+                    words[i]=String.valueOf(ch);
+                    flag=true;
+                }
+                else{
+                    flag=false;
+                    break;
+                }
+            }
+            
+            if(flag){
+                ans.add(String.valueOf(a));
+            }
+        }
+        return ans;
+    }
+}
