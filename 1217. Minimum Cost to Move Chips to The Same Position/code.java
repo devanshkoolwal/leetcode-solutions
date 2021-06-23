3828d0cd2baed73440727bd11846494c1447bbf9
Devansh Koolwal <dkoolwal7@gmail.com>
2021-06-23 22:23:57 +0530

    1217. Minimum Cost to Move Chips to The Same Position

---
+++ 1217. Minimum Cost to Move Chips to The Same Position/code.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int even=0;
+        int odd=0;
+        for(int i:position){
+            if(i%2==0)
+                even++;
+            else
+                odd++;
+        }   
+        
+        return Math.min(odd,even);
+    }
+}
