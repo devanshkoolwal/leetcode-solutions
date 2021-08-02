9e27e80cbbce301c1759a8593e8d5ee0bf0a803a
Devansh Koolwal <dkoolwal7@gmail.com>
2021-08-02 15:03:23 +0530

    169. Majority Element

---
+++ 169. Majority Element/code.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count=0,element=0;
+        for(int i:nums){
+            if(count==0) element=i;
+            if(element==i) count++;
+            else count--;
+        }
+        return element;
+    }
+}
